b55f121f1d3e15f04a2c1c878f000ce10743f982
ssv8317 <ssv8317@gmail.com>
2025-06-20 09:06:40 -0400

    Remove Navbar from Dashboard

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { routes } from './app/app.routes';
 import { NavbarComponent } from './app/components/navbar/navbar.component';
 
@@ -13,13 +13,22 @@ import { NavbarComponent } from './app/components/navbar/navbar.component';
   imports: [CommonModule, RouterOutlet, NavbarComponent],
   template: `
     <div class="min-h-screen bg-gray-50">
-      <app-navbar></app-navbar>
+      <!-- Only show navbar on non-dashboard pages -->
+      <app-navbar *ngIf="!isDashboardPage"></app-navbar>
       <router-outlet></router-outlet>
     </div>
   `,
 })
 export class App {
   name = 'HomeMate';
+  isDashboardPage = false;
+
+  constructor(private router: Router) {
+    // Listen to route changes to determine if we're on dashboard
+    this.router.events.subscribe(() => {
+      this.isDashboardPage = this.router.url === '/dashboard';
+    });
+  }
 }
 
 bootstrapApplication(App, {
