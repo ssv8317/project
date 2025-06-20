6494f248f1027e86f1f621533cc70a16332bae50
ssv8317 <ssv8317@gmail.com>
2025-06-19 23:09:41 -0400

    Remove navbar from main app

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,13 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { routes } from './app/app.routes';
-import { NavbarComponent } from './app/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet],
   template: `
     <div class="min-h-screen bg-gray-50">
-      <app-navbar></app-navbar>
       <router-outlet></router-outlet>
     </div>
   `,
